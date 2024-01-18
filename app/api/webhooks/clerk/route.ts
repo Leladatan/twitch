edf0544a2bfdff9e73a5099902b12fa7aa8f8edf
Leladatan <vov4ik.samurai@yandex.ru>
2024-01-18 12:32:42 +0300

    ref

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,20 +1,19 @@
-import {Webhook} from "svix";
-import {headers} from "next/headers";
-import {WebhookEvent} from "@clerk/nextjs/server";
-import {ReadonlyHeaders} from "next/dist/server/web/spec-extension/adapters/headers";
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
 import {db} from "@/prisma/db";
 
-export const POST = async (req: Request) => {
-  const WEBHOOK_SECRET: string | undefined = process.env.WEBHOOK_SECRET;
+export async function POST(req: Request) {
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error("Please add webhook_secret from Clerk Dashboard to .env or .env.local");
+    throw new Error("Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local");
   }
 
-  const headerPayload: ReadonlyHeaders = headers();
-  const svix_id: string | null = headerPayload.get("svix-id");
-  const svix_timestamp: string | null = headerPayload.get("svix-timespamp");
-  const svix_signature: string | null = headerPayload.get("svix-signature");
+  const headerPayload = headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
 
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response("Error occured -- no svix headers", {
@@ -23,20 +22,20 @@ export const POST = async (req: Request) => {
   }
 
   const payload = await req.json();
-  const body: string = JSON.stringify(payload);
+  const body = JSON.stringify(payload);
 
-  const wh: Webhook = new Webhook(WEBHOOK_SECRET);
+  const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
 
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
-      "svix_timestamp": svix_timestamp,
-      "svix_signature": svix_signature,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
     }) as WebhookEvent;
   } catch (err) {
-    console.error("Error verifying webhook", err);
+    console.error("Error verifying webhook:", err);
     return new Response("Error occured", {
       status: 400
     });
@@ -50,7 +49,7 @@ export const POST = async (req: Request) => {
         externalUserId: payload.data.id,
         username: payload.data.username,
         imageUrl: payload.data.image_url,
-      }
+      },
     });
   }
 
@@ -62,7 +61,7 @@ export const POST = async (req: Request) => {
       data: {
         username: payload.data.username,
         imageUrl: payload.data.image_url,
-      }
+      },
     });
   }
 
@@ -70,9 +69,9 @@ export const POST = async (req: Request) => {
     await db.user.delete({
       where: {
         externalUserId: payload.data.id,
-      }
+      },
     });
   }
 
-  return new Response("Success", {status: 200});
-};
+  return new Response("", { status: 200 });
+}
