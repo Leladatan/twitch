94c4aebcf8992ebfc642c01239ae774abf8d5206
Leladatan <vov4ik.samurai@yandex.ru>
2024-01-18 12:51:23 +0300

    ref

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -4,7 +4,7 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import {db} from "@/prisma/db";
 
 export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOKS_SECRET;
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error("Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local");
