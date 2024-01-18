1b05e8a8fc94fa05b5411aa1cc61742b650bf26a
Leladatan <vov4ik.samurai@yandex.ru>
2024-01-18 12:22:58 +0300

    ref

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,6 @@
-import {db} from "@/prisma/db";
-import {User} from "@prisma/client";
-
-const LayoutRoot = async ({children}: { children: React.ReactNode }) => {
-  const user: User[] = await db.user.findMany();
-
+const LayoutRoot =  ({children}: { children: React.ReactNode }) => {
   return (
     <main>
-      {user[0]?.id}
       {children}
     </main>
   );
